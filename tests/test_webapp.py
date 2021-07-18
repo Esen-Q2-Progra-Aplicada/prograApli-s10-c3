58444d5b73bf7d084df7a71ebdea89199211fa7b
baylagas <balbino_a@hotmail.com>
2021-07-18 13:56:35 -0600

    update tests

--- tests/test_webapp.py
+++ tests/test_webapp.py
@@ -8,5 +8,7 @@ def test_webapp():
 
 
 def test_webapp_site_home():
+    app = Flask(__name__)
+    MainRoutes.configure_routes(app)
     response = requests.get("http://localhost:5000")
     assert response.status_code == 200
