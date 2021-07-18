1cc13de81c63a6c0035fc37300a898100a8acce3
baylagas <balbino_a@hotmail.com>
2021-07-18 13:54:02 -0600

    update tests

--- tests/test_webapp.py
+++ tests/test_webapp.py
@@ -8,5 +8,5 @@ def test_webapp():
 
 
 def test_webapp_site_home():
-    response = requests.get("http://www.google.com")
+    response = requests.get("http://localhost:5000")
     assert response.status_code == 200
