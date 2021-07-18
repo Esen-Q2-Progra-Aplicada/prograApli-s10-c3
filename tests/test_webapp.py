3ab89de0067f554cac5fcb12ecea3f4f4a7a2700
baylagas <balbino_a@hotmail.com>
2021-07-18 13:44:52 -0600

    update tests

--- tests/test_webapp.py
+++ tests/test_webapp.py
@@ -1,2 +1,13 @@
+from flask import Flask
+from routes.main_routes import MainRoutes
+import requests
+
+
 def test_webapp():
     assert True is True
+
+
+def test_webapp_site_home():
+    response = requests.get("http://127.0.0.1:5000/")
+    print(response)
+    assert response.status_code == 200
